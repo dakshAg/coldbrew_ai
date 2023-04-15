cce56a9342d74322856a83a899aeaf4d0a4dd13e
Eunji <kimek@student.unimelb.edu.au>
2023-04-15 12:53:50 +1000

    prompt change in disposal

--- metrics.py
+++ metrics.py
@@ -16,7 +16,7 @@ def ai_disposal(prod_name, lga_url):
     # We need openai to classift the ingredients into categories that match the waste disposal guide
     # We will use the openai api to do this
     response = gpt.get_response(0.5,
-        "First classify this" + prod_name + "into a general category on the following website then use information from this site to tell me what I should do with it:" + lga_url)
+        "First classify this product (" + prod_name + ") into a general category on the following website then use information from this site to tell me what I should do with it in 100 words:" + lga_url)
     return response
 
 # Using the data, we will create a function that will take in a product name and output a sustainability score
@@ -24,7 +24,7 @@ def ai_cf_wf(prod_name):
     # We need openai to classify the ingredients into categories that match the waste disposal guide
     # We will use the openai api to do this
     response_carbon = gpt.get_response(0.2,
-        "Please rate the sustainability of " + prod_name + " on a scale of 1 to 10, with 1 being not sustainable at all and 10 being extremely sustainable. Please take into consideration factors such as the materials used, the manufacturing process, the products lifespan, and its impact on the environment.")
+        "Please rate the sustainability of this product (" + prod_name + ") on a scale of 1 to 10, with 1 being not sustainable at all and 10 being extremely sustainable. Please take into consideration factors such as the materials used, the manufacturing process, the products lifespan, and its impact on the environment.")
     response_water = gpt.get_response(0.2,
         "Please rate the sustainability of " + prod_name + " on a scale of 1 to 10, with 1 being not sustainable at all and 10 being extremely sustainable. Please take into consideration factors such as the materials used, the manufacturing process, the products lifespan, and its impact on the environment.")
     # Extract the numbers from the response
