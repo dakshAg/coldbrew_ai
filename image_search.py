2360e4a0e99217a27d6a4b2ba7aabc85ecac082a
dakshagrawal <dakshagrawalx@gmail.com>
2023-04-15 14:50:13 +1000

    Secret

--- image_search.py
+++ image_search.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-import chromedriver_binary
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
 
 def image_search(image_path):
     """This function will take an image path and return the top 5 search results from google vision
@@ -13,27 +10,22 @@ def image_search(image_path):
     Returns:
         list: List of dictionaries comprised of name, image link and url.
     """
+    #path of webdriver executable
+    webdriver_path = "/image_search"
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.binary_location = chromedriver_binary.chromedriver_filename
-    chrome_driver_binary = "/usr/local/bin/chromedriver"
-
-    # new Chrome webdriver instance
-    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=chrome_options)
+    #new Chrome webdriver instance
+    driver = webdriver.Chrome(webdriver_path)
     driver.get("https://www.google.com/imghp")
 
     # find the "Search by image" icon and click it
-    search_by_image_icon = driver.find_element(By.XPATH,
-                                               "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]")
+    search_by_image_icon = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[3]/div[3]")
     search_by_image_icon.click()
 
     # wait for the search options to appear
     driver.implicitly_wait(3)
 
     # click on the "Paste image link" option
-    paste_image_link_option = driver.find_element(By.XPATH,
-                                                  '//*[@id="ow6"]/div[3]/c-wiz/div[2]/div/div[3]/div[2]/c-wiz/div[2]/input')
+    paste_image_link_option = driver.find_element(By.XPATH, '//*[@id="ow6"]/div[3]/c-wiz/div[2]/div/div[3]/div[2]/c-wiz/div[2]/input')
     paste_image_link_option.click()
 
     # paste the image link
@@ -47,16 +39,12 @@ def image_search(image_path):
     # wait for the search results to load
     driver.implicitly_wait(5)
 
-    # save the first five search results by its url and image link
+    #save the first five search results by its url and image link
     top5_search_results = []
     for i in range(1, 6):
-        href_result = driver.find_element(By.XPATH,
-                                          '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[1]/div[{}]/div/a'.format(
-                                              i))
+        href_result = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[1]/div[{}]/div/a'.format(i))
         url = href_result.get_attribute("href")
-        image_result = driver.find_element(By.XPATH,
-                                           '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[1]/div[{}]/div/a/div'.format(
-                                               i))
+        image_result = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[2]/div/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[1]/div[{}]/div/a/div'.format(i))
         image_link = image_result.get_attribute("data-thumbnail-url")
         product_name = image_result.get_attribute("data-item-title")
         top5_search_results.append({"product": product_name, "image": image_link, "url": url})
---
+++ secret.py
@@ -0,0 +1,3 @@
+def OPENAI_API():
+    # Import the OpenAI API key from secret.py
+    return "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"
